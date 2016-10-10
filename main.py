86fcf34f9ab5a9ed894e7363715190fc37605ecd
Alpvax <alpvax@netscape.net>
2016-10-10 18:43:30 +0100

    Removed AppEngine files

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import vendor
+# insert `lib` as a site directory so our `main` module (this) can load
+# third-party libraries, and override built-ins with newer
+# versions.
+vendor.add('lib')
+
+
 import telegram
 from flask import Flask, request, json
 from google.appengine.api import urlfetch
