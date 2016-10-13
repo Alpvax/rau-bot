3aa0d8f2f68da73cf2b110b4681a31a24f3e51bb
Alpvax <alpvax@netscape.net>
2016-10-13 02:38:23 +0100

    Fixed simpleHandler

--- handlers.py
+++ handlers.py
@@ -1,22 +1,37 @@
-def simpleHandler(func, handlerCallable, key=None, **kwargs):
-    kwargs['callback'] = func
-    handler = handlerCallable(key or func.__name__, **kwargs)
-    func.handler = handler
-    return func
+class simpleHandler(object):
+    def __init__(self, handlerCallable, key=None, **kwargs):
+        import sys
+        self.module = sys.modules[self.__module__]
+        if not hasattr(self.module, "ALL_HANDLERS"):
+            setattr(self.module, "ALL_HANDLERS", [])
+        self.handlerCallable = handlerCallable
+        self.key = key
+        self.kwargs = kwargs
+        
+    def __call__(self, callable):
+        self.kwargs['callback'] = callable
+        handler = self.handlerCallable(self.key or callable.__name__, **self.kwargs)
+        callable.handler = handler
+        self.module.ALL_HANDLERS.append(callable)
+        return callable
 
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
     
+#==================Actual Handlers==================#
+    
 from telegram.ext import CommandHandler, MessageHandler, Filters
+from telegram import ForceReply
     
 @simpleHandler(CommandHandler)
 def start(bot, update):
-    bot.sendMessage(update.message.chat_id, text='Hi!')
+    user = update.message.from_user
+    bot.sendMessage(update.message.chat_id, text="Hi {}!\nHow are you?".format(('{} {}'.format(user.first_name, user.last_name)) if user.last_name else user.first_name))#, reply_to_message_id=update.message.message_id, reply_markup=ForceReply(selective=True))
 
 @simpleHandler(CommandHandler)
 def help(bot, update):
     bot.sendMessage(update.message.chat_id, text='Help!')
 
-@simpleHandler(MessageHandler, Filters.text)
-def echo(bot, update):
-    bot.sendMessage(update.message.chat_id, text=update.message.text)
+@simpleHandler(CommandHandler, pass_args=True)#Use "(MessageHandler, [Filters.text])" to recognise any (text) response
+def echo(bot, update, **args):
+    bot.sendMessage(update.message.chat_id, text=" ".join(args["args"]))
--- main.py
+++ main.py
@@ -1,82 +1,30 @@
-import vendor
-# insert `lib` as a site directory so our `main` module (this) can load
-# third-party libraries, and override built-ins with newer
-# versions.
-vendor.add('lib')
-
-
 import telegram
-from flask import Flask, request, json
-from google.appengine.api import urlfetch
-app = Flask(__name__)
-app.config['DEBUG'] = True
 
-from credentials import TOKEN, APP_URL
+from credentials import TOKEN
 
-global bot
-bot = telegram.Bot(token=TOKEN)
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-from telegram.ext import Dispatcher
-dispatcher = None
+logger = logging.getLogger(__name__)
 
-def setup():
-    '''GAE DISPATCHER SETUP'''
-
-    global dispatcher
-    # Note that update_queue is setted to None and
-    # 0 workers are allowed on Google app Engine (If not-->Problems with multithreading)
-    dispatcher = Dispatcher(bot=bot, update_queue=None, workers=0)
-    
-    import handlers
-    
-    # ---Register handlers---
-    for callback in handlers:
-        if callback.hasattr("handler"):
-            dispatcher.add_handler(callback.handler)
-
-#    # ---Register handlers here---
-#    dispatcher.add_handler(CommandHandler("start", start))
-#    dispatcher.add_handler(CommandHandler("help", help))
-#    dispatcher.add_handler(MessageHandler([Filters.text], echo))
-    dispatcher.add_error_handler(handlers.error)
-
-    return dispatcher
-
-@app.route('/' + TOKEN,methods=['POST'])
-def telegram_webhook():
-    if request.method == "POST":
-        # retrieve the message in JSON and then transform it to Telegram object
-        update = telegram.Update.de_json(request.get_json(force=True))
+def addHandler(dispatcher, *handlers):
+    for obj in handlers:
+        if hasattr(obj, "handler"):
+            dispatcher.add_handler(obj.handler)
         
-        # Manually get updates and pass to dispatcher
-        dispatcher.process_update(update)
-#
-#        chat_id = update.message.chat.id
-#
-#        # Telegram understands UTF-8, so encode text for unicode compatibility
-#        text = update.message.text.encode('utf-8')
-#
-#        # repeat the same message back (echo)
-#        bot.sendMessage(chat_id=chat_id, text=text)
-
-    return 'ok'
-
-@app.route('/set_webhook', methods=['GET', 'POST'])
-def set_webhook():
-    setup()
-    s = bot.setWebhook(APP_URL + '/' + TOKEN)
-    if s:
-        return "WebHook setup ok"
-    else:
-        return "WebHook setup failed"
-
-@app.route('/')
-def index():
-    return "<!DOCTYPE html><html><head><script>window.location.href='http://telegram.me/rau_bot'</script></head> \
-    <body>If you are not redirected automatically, <a href='http://telegram.me/rau_bot'>click here</a></body></html>"
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    """Return a custom 404 error."""
-    return 'Sorry, nothing at this URL.', 404
+def main():
+    from telegram.ext import Updater
+    updater = Updater(token=TOKEN)
+    dispatcher = updater.dispatcher
+    import handlers
+    dispatcher.add_error_handler(handlers.error)
+    addHandler(dispatcher, *handlers.ALL_HANDLERS)
+    
+    updater.start_polling()
+    # Run the bot until the you presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+    
+if __name__ == "__main__":
+    main()
