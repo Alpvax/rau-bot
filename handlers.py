f41772ce36b7bbe67625537b1802b571cf413f91
Alpvax <alpvax@netscape.net>
2016-10-20 01:28:41 +0100

    Fixed remotely stopping/reloading bot

--- handlers.py
+++ handlers.py
@@ -1,46 +1,114 @@
-class simpleHandler(object):
-    def __init__(self, handlerCallable, key=None, **kwargs):
+class updateHandler(object):
+    def __init__(self, handlerConstructor):
+        self.constructHandler = handlerConstructor
         import sys
         self.module = sys.modules[self.__module__]
         if not hasattr(self.module, "ALL_HANDLERS"):
             setattr(self.module, "ALL_HANDLERS", [])
-        self.handlerCallable = handlerCallable
-        self.key = key
-        self.kwargs = kwargs
         
     def __call__(self, callable):
-        self.kwargs['callback'] = callable
-        handler = self.handlerCallable(self.key or callable.__name__, **self.kwargs)
+        handler = self.constructHandler(callable)
         callable.handler = handler
         self.module.ALL_HANDLERS.append(callable)
         return callable
+        
+class ConversationHandler(object):
+    END = -1
+    def __init__(self):
+        self.entry_points = []
+        self.states = {}
+        self.fallbacks = []
+        from telegram.ext import ConversationHandler as _ConvHandler
+        import sys
+        self.module = sys.modules[self.__module__]
+        if not hasattr(self.module, "ALL_HANDLERS"):
+            setattr(self.module, "ALL_HANDLERS", [])
+        self.module.ALL_HANDLERS.append(self)
+        
+    def entry(self, handlerConstructor):
+        def decorator(func):
+            self.entry_points.append(handlerConstructor(func))
+            return func
+        return decorator
+        
+    def state(self, index, handlerConstructor):
+        def decorator(func):
+            func.state = index
+            try:
+                self.states[index].append(handlerConstructor(func))
+            except KeyError:
+                self.states[index] = [handlerConstructor(func)]
+            return func
+        return decorator
+        
+    def fallback(self, handlerConstructor):
+        def decorator(func):
+            self.fallbacks.append(handlerConstructor(func))
+            return func
+        return decorator
+        
+    @property
+    def handler(self):
+        from telegram.ext import ConversationHandler as _ConvHandler
+        return _ConvHandler(entry_points=self.entry_points, states=self.states, fallbacks=self.fallbacks)
+        
+###        # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+###    conv_handler = ConversationHandler(
+###        entry_points=[CommandHandler('start', start)],
+###
+###        states={
+###            GENDER: [RegexHandler('^(Boy|Girl|Other)$', gender)],
+###
+###            PHOTO: [MessageHandler([Filters.photo], photo),
+###                    CommandHandler('skip', skip_photo)],
+###
+###            LOCATION: [MessageHandler([Filters.location], location),
+###                       CommandHandler('skip', skip_location)],
+###
+###            BIO: [MessageHandler([Filters.text], bio)]
+###        },
+###
+###        fallbacks=[CommandHandler('cancel', cancel)]
+###    )
 
-def logger():
+def getLogger():
     global logger
     try:
         return logger
     except:
-        import sys
-        logger = sys.modules["__main__"].logger
+        import logging
+        logger = logging.getLogger("__main__")
         return logger
         
 def error(bot, update, error):
-    logger().warn('Update "%s" caused error "%s"' % (update, error))
+    getLogger().warn('Update "%s" caused error "%s"' % (update, error))
     
+
+#=======Use from handlers import * to import========#
+#======= all handler types from telegram.ext========#
+from telegram.ext import CallbackQueryHandler, ChosenInlineResultHandler, CommandHandler, InlineQueryHandler, MessageHandler, RegexHandler, StringCommandHandler, StringRegexHandler, TypeHandler
+
+__all__ = ["CallbackQueryHandler", "ChosenInlineResultHandler", "CommandHandler", "InlineQueryHandler", "MessageHandler", "RegexHandler", "StringCommandHandler", "StringRegexHandler", "TypeHandler", "ALL_HANDLERS", "updateHandler", "ConversationHandler"]
+    
+#===================================================#
 #==================Actual Handlers==================#
     
-from telegram.ext import CommandHandler, MessageHandler, Filters
-from telegram import ForceReply
+from telegram import InlineQueryResultArticle, InputTextMessageContent, ReplyKeyboardMarkup#, ForceReply
+from uuid import uuid4
     
-@simpleHandler(CommandHandler)
+@updateHandler(lambda f: CommandHandler(f.__name__, f))
 def start(bot, update):
     user = update.message.from_user
     bot.sendMessage(update.message.chat_id, text="Hi {}!\nHow are you?".format(('{} {}'.format(user.first_name, user.last_name)) if user.last_name else user.first_name))#, reply_to_message_id=update.message.message_id, reply_markup=ForceReply(selective=True))
 
-@simpleHandler(CommandHandler)
+@updateHandler(lambda f: CommandHandler(f.__name__, f))
 def help(bot, update):
-    bot.sendMessage(update.message.chat_id, text='Help!')
+    bot.sendMessage(update.message.chat_id, text='Help!\n<Help topics>')
 
-@simpleHandler(CommandHandler, pass_args=True)#Use "(MessageHandler, [Filters.text])" to recognise any (text) response
+@updateHandler(lambda f: CommandHandler(f.__name__, f, pass_args=True))#Use "(MessageHandler, [Filters.text])" to recognise any (text) response
 def echo(bot, update, **args):
     bot.sendMessage(update.message.chat_id, text=" ".join(args["args"]))
+        
+#@updateHandler(lambda f: InlineQueryHandler(f, pattern="\s*/echo\s+(?P<text>.+)", pass_groupdict=True))
+#def inlineEcho(bot, update, **args):
+#    pass
---
+++ launcher.py
@@ -0,0 +1,148 @@
+def unimport(*names):
+    import sys
+    if not names:
+        import os.path
+        names = [k for k,v in sys.modules.items() if not v or (hasattr(v, "__file__") and os.path.exists(os.path.basename(v.__file__)))]
+    for name in names:
+        del sys.modules[name]
+
+def setupLogging(name=__name__, logFile=None):
+    if not logFile:
+        logFile = name + ".log"
+    import logging, sys
+    rootLogger = logging.getLogger("")
+    rootLogger.setLevel(logging.DEBUG)
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler(logFile)
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    rootLogger.addHandler(fh)
+    # create console handler with a higher log level
+    ch = logging.StreamHandler(sys.stderr)
+    ch.setLevel(logging.ERROR)
+    ch.setFormatter(logging.Formatter('%(name)s - %(levelname)s - %(message)s'))
+    rootLogger.addHandler(ch)
+    return logging.getLogger(name)
+
+logger = setupLogging(logFile="rau-bot.log")
+
+
+class Controller(object):
+    def __init__(self, *setup):
+        from threading import Event
+        self.stop_event = Event()
+        self.start_event = Event()
+        self.running = False
+        self.updater = None
+        self.setupFuncs = setup or []
+        
+    def _run(self):
+        from time import sleep
+        while self.running:
+            if self.stop_event.isSet():
+                self.stop_event.clear()
+                print("Stopping...")
+                if self.updater:
+                    self.updater.stop()
+                    self.updater = None
+                print("Bot stopped")
+                unimport("handlers", "credentials")
+            if self.start_event.isSet():
+                self.start_event.clear()
+                if not self.updater:
+                    self.setup()
+                print("Starting rau_bot in long polling mode...")
+                self.updater.start_polling()
+                
+    def setup(self):
+        [func() for func in self.setupFuncs]
+        import credentials, handlers
+        from telegram.ext import Updater
+        self.updater = Updater(credentials.TOKEN)
+        self.updater.dispatcher.add_error_handler(handlers.error)
+        self.addHandler(*handlers.ALL_HANDLERS)
+        from signal import signal, SIGINT, SIGTERM, SIGABRT
+        for sig in (SIGINT, SIGTERM, SIGABRT):
+            signal(sig, self.updater.signal_handler)
+        
+    def addHandler(self, *handlers):
+        for obj in handlers:
+            if hasattr(obj, "handler"):
+                self.updater.dispatcher.add_handler(obj.handler)
+                
+    def start(self):
+        self.start_event.set()
+        if not self.running:
+            self.running = True
+            self._run()
+        
+    def stop(self):
+        self.stop_event.set()
+        self.running = False
+        
+    def reload(self):
+        self.stop_event.set()
+        self.start_event.set()
+
+def createBotStateHandler():
+    from handlers import ConversationHandler, CommandHandler, RegexHandler
+    from telegram import ReplyKeyboardMarkup
+    bot_state_handler = ConversationHandler()
+    bot_state_handler.handlerHelpers = []
+    
+    def bot_state_helper(matcher=None):
+        def decorator(func):
+            func.matcher = matcher or (lambda cmd: (cmd[0].lower() == func.__name__, (len(cmd) > 1 and cmd[1:]) or []))
+            bot_state_handler.handlerHelpers.append(func)
+            return func
+        return decorator
+        
+    @bot_state_helper()
+    def terminate(bot, update, args):
+        update.message.reply_text("Stopping bot! Cannot be restarted remotely!\nAre you sure?", reply_markup=ReplyKeyboardMarkup([["Yes", "No"]], one_time_keyboard=True))
+        return verifystop.state
+            
+    @bot_state_helper()
+    def reload(bot, update, args):
+        update.message.reply_text("Reloading bot (args=\"{0}\"). If something goes wrong, you will need to manually restart!\nAre you sure?".format(args), reply_markup=ReplyKeyboardMarkup([["Yes", "No"]], one_time_keyboard=True))
+        return verifyreload.state
+
+    @bot_state_handler.entry(lambda f: CommandHandler(f.__name__, f, pass_args=True))
+    def bot_state(bot, update, **args):
+        command = args["args"]
+        if len(command) < 1:
+            update.message.reply_text("No command recognised!")
+            return ConversationHandler.END
+        if update.message.from_user.username.lower() != "alpvax":
+            err = "User {0} attempted to change bot state ({1})!".format(update.message.from_user.name, " ".join(command))
+            update.message.reply_text(err)
+            getLogger().warn(err)
+            return ConversationHandler.END
+        
+        for helper in bot_state_handler.handlerHelpers:
+            m, cmdargs = helper.matcher(command)
+            if m:
+                return helper(bot, update, cmdargs)
+            
+        update.message.reply_text("Command \"{0}\" not recognised!".format(" ".join(args["args"])))
+        return ConversationHandler.END
+            
+        
+    @bot_state_handler.state("terminate", lambda f: RegexHandler('^(Yes|No)$', f))
+    def verifystop(bot, update):
+        update.message.reply_text("Bot Stopped! Manual restart required.")
+        if update.message.text == "Yes":
+            controller.stop()
+            
+    @bot_state_handler.state("reload", lambda f: RegexHandler('^(Yes|No)$', f))
+    def verifyreload(bot, update):
+        update.message.reply_text("Reloading bot.")
+        if update.message.text == "Yes":
+            controller.reload()
+
+if __name__=="__main__":
+    #createBotStateHandler()
+    controller = Controller(createBotStateHandler)
+    #import thread
+    #thread.start_new_thread(lambda controller: [raw_input(), controller.reload()], (controller,))
+    controller.start()
